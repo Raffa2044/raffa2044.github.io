2ef307c1743eb1eea5464b76051676471017174a
Raffa2044 <147381748+Raffa2044@users.noreply.github.com>
2024-10-02 11:21:17 +0200

    Add files via upload

--- pages/primo-js/index.js
+++ pages/primo-js/index.js
@@ -1,6 +1,24 @@
 var input = document.getElementById("sium");
 var testo = document.getElementById("testo");
+var bulb = document.getElementById("bulb");
+var cc = document.getElementById("colorecustom");
 
 function premuto() {
     testo.innerHTML = "Testo: " + input.value;
+}
+
+function lightOn() {
+    bulb.setAttribute("src", "./assets/on.png")
+}
+
+function lightOff() {
+    bulb.setAttribute("src", "./assets/off.png")
+}
+
+function ccOff() {
+    cc.setAttribute("style", "display: none;");
+}
+
+function ccOn() {
+    cc.setAttribute("style", "");
 }
